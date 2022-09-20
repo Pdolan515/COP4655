f38f0e399b2f9320a478f971552ef638c31c8aff
Pdolan515 <111931484+Pdolan515@users.noreply.github.com>
2022-09-20 19:25:21 -0400

    Add files via upload

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,25 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by patrick dolan on 9/13/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by patrick dolan on 9/14/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
